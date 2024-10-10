2542a1e59b3b69104ae753cfa921582f1c0ccf9f
habibur62 <habibur.vendabari@gmail.com>
2024-10-10 12:42:33 +0600

    routers file create and connect with App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-red-600 text-3xl font-bold underline">
-        MERN app
-      </div>
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import * as React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider} from 'react-router-dom'
+import router from './routers'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
